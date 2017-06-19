5f9b6d35efe348ae03d052491c13dbfb8538c11c
Mike Nelson Jr <michaeljr.nelson@gmail.com>
2017-06-19 08:00:21 -0400

    Initial commit

---
+++ HeadingHome/HeadingHomeTests/HeadingHomeTests.swift
@@ -0,0 +1,36 @@
+//
+//  HeadingHomeTests.swift
+//  HeadingHomeTests
+//
+//  Created by Mike Nelson on 6/19/17.
+//
+//
+
+import XCTest
+@testable import HeadingHome
+
+class HeadingHomeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
